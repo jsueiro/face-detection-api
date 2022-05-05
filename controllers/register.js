7920cb495351454c2eddfc4e8d20016d4174a50f
jsueiro <juan.sueiro@outlook.com>
2022-05-05 22:33:11 +0200

    bugfix destructuring body

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,11 @@
 const handleRegister = (req, res, pg, bcrypt) => {
+    const { email, name, password } = req.body;
 
     if (!email || !name || !password) {
         // return 400 to stop execution if empty form
         return res.status(400).json('incorrect form submission');
     }
 
-    const { email, name, password } = req.body;
 
     const hash = bcrypt.hashSync(password);
 
