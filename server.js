567588cde644f38d42d2495ef452bfa6f83db419
jsueiro <juan.sueiro@outlook.com>
2022-05-05 22:25:58 +0200

    add db connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image')
 const pg = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'juan',
-        password: '',
-        database: 'face-detection'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
